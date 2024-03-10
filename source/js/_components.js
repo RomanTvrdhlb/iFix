a4c3ad5c02d6dee2874683e468610387606a8cc2
roman <romasaab@gmail.com>
2024-03-11 00:26:47 +0200

    add aos

--- source/js/_components.js
+++ source/js/_components.js
@@ -5,7 +5,7 @@ import './components/getDinamicHeight';
 import './components/accordions';
 import './components/replaceEl';
 import './components/modals';
-// import './components/aos';
+import './components/aos';
 // import './components/ajaxContent';
 // import './components/form';
 
